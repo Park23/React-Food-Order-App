10db6e08ea5e42636ebf4ed6e055bf4bb1225879
Parker Wilson <Parkerwilson1169@gmail.com>
2022-03-28 11:44:10 -0500

    commit

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+
+const Cart = (props) => {
+  const [showCheckout, setShowCheckout] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+  const orderHandler = () => {
+    setShowCheckout(!showCheckout);
+  };
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {" "}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {showCheckout && <Checkout onCancel={orderHandler} />}
+      <div className={classes.actions}>
+        {!showCheckout && (
+          <button className={classes["button--alt"]} onClick={props.onHideCart}>
+            Close
+          </button>
+        )}
+        {hasItems && !showCheckout && (
+          <button onClick={orderHandler} className={classes.button}>
+            Order
+          </button>
+        )}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
